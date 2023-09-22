50181c22a2d6e7b552c67faa6779e1219aa9ac35
benbencik <bencik42@gmail.com>
2023-09-22 17:33:29 +0200

    modify main to read params from file

--- merkle-tree/src/main.rs
+++ merkle-tree/src/main.rs
@@ -1,4 +1,5 @@
 use ark_bls12_381::{Bls12_381, Fr};
+use ark_ec::bls12::Bls12;
 use ark_ed_on_bls12_381::EdwardsParameters;
 use ark_poly_commit::PolynomialCommitment;
 use ark_std::rand::RngCore;
@@ -6,15 +7,22 @@ use ark_std::{test_rng, UniformRand};
 use merkle_tree::HEIGHT;
 use merkle_tree::{MerkleTree, MerkleTreeCircuit};
 use plonk_core::commitment::KZG10;
-use plonk_core::prelude::{
-    verify_proof, Circuit, StandardComposer, VerifierData,
-};
+use plonk_core::prelude::{verify_proof, Circuit, StandardComposer, VerifierData};
 use plonk_core::proof_system::Prover;
 use plonk_hashing::poseidon::constants::PoseidonConstants;
 use plonk_hashing::poseidon::poseidon_ref::NativeSpecRef;
+mod io;
 
 fn main() {
+    let generate_setup = false;
     let mut rng = test_rng();
+
+    if generate_setup {
+        let size = 1 << (HEIGHT + 9);
+        let pp = KZG10::<Bls12_381>::setup(size, None, &mut rng).unwrap();
+        io::store_params(pp, "merkle-tree/setup/setup2");
+        return;
+    }
 
     let param = PoseidonConstants::<Fr>::generate::<3>();
 
@@ -22,13 +30,8 @@ fn main() {
     // first we build a merkle tree
     // ==============================
 
-    let leaf_nodes = (0..1 << (HEIGHT - 1))
-        .map(|_| Fr::rand(&mut rng))
-        .collect::<Vec<_>>();
-    let tree = MerkleTree::<NativeSpecRef<Fr>>::new_with_leaf_nodes(
-        &param,
-        &leaf_nodes,
-    );
+    let leaf_nodes = (0..1 << (HEIGHT - 1)).map(|_| Fr::rand(&mut rng)).collect::<Vec<_>>();
+    let tree = MerkleTree::<NativeSpecRef<Fr>>::new_with_leaf_nodes(&param, &leaf_nodes);
 
     let index = rng.next_u32() % (1 << (HEIGHT - 1));
     let proof = tree.gen_proof(index as usize);
@@ -59,7 +62,8 @@ fn main() {
     {
         // public parameters
         let size = 1 << (HEIGHT + 9);
-        let pp = KZG10::<Bls12_381>::setup(size, None, &mut rng).unwrap();
+        // let pp = KZG10::<Bls12_381>::setup(size, None, &mut rng).unwrap();
+        let pp: ark_poly_commit::kzg10::UniversalParams<Bls12<ark_bls12_381::Parameters>> = io::load_params("merkle-tree/setup/setup2");
 
         let mut dummy_circuit = MerkleTreeCircuit {
             param: param.clone(),
@@ -67,32 +71,21 @@ fn main() {
         };
 
         // proprocessing
-        let (pk, (vk, _pi_pos)) =
-            dummy_circuit.compile::<KZG10<Bls12_381>>(&pp).unwrap();
+        let (pk, (vk, _pi_pos)) = dummy_circuit.compile::<KZG10<Bls12_381>>(&pp).unwrap();
 
         // proof generation
-        let leaf_nodes = (0..1 << (HEIGHT - 1))
-            .map(|_| Fr::rand(&mut rng))
-            .collect::<Vec<_>>();
-        let tree = MerkleTree::<NativeSpecRef<Fr>>::new_with_leaf_nodes(
-            &param,
-            &leaf_nodes,
-        );
+        let leaf_nodes = (0..1 << (HEIGHT - 1)).map(|_| Fr::rand(&mut rng)).collect::<Vec<_>>();
+        let tree = MerkleTree::<NativeSpecRef<Fr>>::new_with_leaf_nodes(&param, &leaf_nodes);
         let mut real_circuit = MerkleTreeCircuit {
             param: param.clone(),
             merkle_tree: tree,
         };
         let now = std::time::Instant::now();
         let (proof, pi) = {
-            let mut prover =
-                Prover::<Fr, EdwardsParameters, KZG10<Bls12_381>>::new(
-                    b"Merkle tree",
-                );
+            let mut prover = Prover::<Fr, EdwardsParameters, KZG10<Bls12_381>>::new(b"Merkle tree");
             real_circuit.gadget(prover.mut_cs()).unwrap();
 
-            real_circuit
-                .gen_proof::<KZG10<Bls12_381>>(&pp, pk, b"Merkle tree")
-                .unwrap()
+            real_circuit.gen_proof::<KZG10<Bls12_381>>(&pp, pk, b"Merkle tree").unwrap()
         };
         println!("The prove generation time is {:?}", now.elapsed());
 
